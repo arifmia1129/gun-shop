6c62c91fecdb7fb6a766b2e68bbd0eee4c1a4da7
arifvtti <arif.vtti@gmail.com>
2022-03-24 19:57:49 +0600

    first commit

---
+++ src/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <h3>Gun Shop</h3>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
